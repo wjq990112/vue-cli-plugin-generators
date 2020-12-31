70e47c01b6fbf78f68004806268403c3f42635af
wjq990112 <1163585385@qq.com>
2020-12-31 23:28:13 +0800

    chore: update version

--- src/add-page.js
+++ src/add-page.js
@@ -12,6 +12,4 @@ const inquirer = require('inquirer');
 const log = require('./utils/log');
 const suffix = require('./utils/suffix');
 
-module.exports = async (api) => {
-  // TODO: 添加创建页面逻辑
-};
+module.exports = async (api) => {};
